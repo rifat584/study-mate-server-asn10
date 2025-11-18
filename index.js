fd2f42954c56e64095993052a1ad9bf6db2732c2
rifat584 <nowucme34@gmail.com>
2025-11-18 15:30:55 +0600

    connected mongodb atlas

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.port || 3000;
-const cors = require('cors');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// middlewere
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-  res.send("server started")
-})
+// mongodb
+const uri =
+  "mongodb+srv://studymate_db:CbOeiVLs3g48ErJG@cluster0.rom6hen.mongodb.net/?appName=Cluster0";
 
-app.listen(port, (req, res)=>{
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("studymate")
+    const partnerColl = db.collection("partners")
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server started");
+});
+
+app.listen(port, (req, res) => {
   console.log("server side started", port);
-})
+});
